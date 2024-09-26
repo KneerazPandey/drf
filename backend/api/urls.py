ccf8321c84457df178b891461de01311c56883fa
KneerazPandey <nirajpandeyniraj77@gmail.com>
2024-09-26 13:44:34 +0000

    added new app products and used the product model instance as api response

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . views import api_home
+from . views import api_home, api_product
 
 
 urlpatterns = [
-    path('', api_home, name='api-home'),
+    # path('', api_home, name='api-home'),
+    path('', api_product, name='api-product'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,7 @@
 import json
-from django.http import HttpRequest, JsonResponse
+from django.http import HttpRequest, JsonResponse, HttpResponse
+from products.models import Product
+from django.forms.models import model_to_dict
 
 
 def api_home(request: HttpRequest):
@@ -15,3 +17,18 @@ def api_home(request: HttpRequest):
     data['headers'] = dict(request.headers)
     data['content-type'] = request.content_type
     return JsonResponse(data)
+
+
+def api_product(request: HttpRequest):
+    model_data = Product.objects.all().order_by("?").first()
+    data = {}
+    if model_data:
+        # data['id'] = model_data.pk
+        # data['title'] = model_data.title 
+        # data['content'] = model_data.content
+        # data['price'] = model_data.price
+        data = model_to_dict(model_data, fields=['id', 'title'])
+    return JsonResponse(data)
+    #     data = model_to_dict(model_data, fields=['id', 'title'])
+    #     data = json.dumps(data)
+    # return HttpResponse(data, headers={'content-type': 'application/json'})
